a9ec7dab1090dfad70f26e0175bd953f20ef86d1
Daniel Chandra <127204428+danielchandra@users.noreply.github.com>
2023-03-09 18:25:48 +0700

    Delete Java, Object Oriented Programming/Project Java Creating Menu with OOP Concept directory

--- Java, Object Oriented Programming/Project Java Creating Menu with OOP Concept/src/pembeli.java
+++
@@ -1,9 +0,0 @@
-
-public class pembeli extends datacustomer {
-
-	public pembeli(String nama, String gender, String password) {
-		super(nama, gender, password);
-		// TODO Auto-generated constructor stub
-	}
-
-}
