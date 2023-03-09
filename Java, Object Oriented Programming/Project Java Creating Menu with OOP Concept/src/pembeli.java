2af83b86a0ddece1d17518d3f739aca7af8d83d0
Daniel Chandra <127204428+danielchandra@users.noreply.github.com>
2023-03-09 18:21:03 +0700

    Add files via upload

---
+++ Java, Object Oriented Programming/Project Java Creating Menu with OOP Concept/src/pembeli.java
@@ -0,0 +1,9 @@
+
+public class pembeli extends datacustomer {
+
+	public pembeli(String nama, String gender, String password) {
+		super(nama, gender, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
